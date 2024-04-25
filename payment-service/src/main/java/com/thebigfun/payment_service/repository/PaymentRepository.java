c56fc0a07384d07c3203a5b6501c2f31026a840c
JohnPaulM@m@ni <u201919105@upc.edu.pe>
2024-04-24 23:26:23 -0500

    Impl the microservicios and JsonFora¡mat pattern

---
+++ payment-service/src/main/java/com/thebigfun/payment_service/repository/PaymentRepository.java
@@ -0,0 +1,1 @@
+package com.thebigfun.payment_service.repository;import com.thebigfun.payment_service.model.Payment;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;@Repositorypublic interface PaymentRepository extends JpaRepository <Payment, Long>{}
